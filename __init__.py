b32ce572e5216f9875c88049e31688209fc04514
Jaiswal <Deepakraj.Jaiswal@lumen.com>
2021-09-26 22:47:28 +0530

    Test

---
+++ __init__.py
@@ -0,0 +1,32 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask, request
+from flask_cors import CORS
+from Chatbot import *
+
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+CORS(app)
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return 'Hello World'
+
+
+@app.route('/get')
+# ‘/’ URL is bound with hello_world() function.
+def get_bot_response():
+    user_text = request.args.get("msg")
+    return str(chat(user_text))
+
+
+# main driver function
+if __name__ == '__main__':
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(host="localhost", port=80, debug=True)
