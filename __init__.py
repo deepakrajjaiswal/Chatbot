3b2e381b52d9a05ed88023f7e998b42f01f525b4
Deepak Jaiswal <mrjaiswaldeepak@gmail.com>
2021-09-27 10:46:31 +0530

    Updated main function

--- __init__.py
+++ __init__.py
@@ -24,4 +24,8 @@ def get_bot_response():
     user_text = request.args.get("msg")
     return str(chat(user_text))
 
-
+# main driver function
+if __name__ == '__main__':
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(host="localhost", port=8000, debug=True)
